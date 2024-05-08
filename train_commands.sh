fa60d0f8d6946e8125277c04aff71ea0ad4cc540
kevin <99797913+blackline0911@users.noreply.github.com>
2024-05-08 12:11:14 +0800

    Update train_commands.sh

--- train_commands.sh
+++ train_commands.sh
@@ -1,3 +1,6 @@
-python fast_reid/datasets/generate_AICUP_patches.py --data_path /mnt/c/Users/kevin/Desktop/ai_cup/32_33_train_v2/train
-python yolov7/tools/AICUP_to_YOLOv7.py --AICUP_dir /mnt/c/Users/kevin/Desktop/ai_cup/32_33_train_v2/train --YOLOv7_dir /mnt/c/Users/kevin/Desktop/ai_cup/32_33_train_v2/yolo
+python fast_reid/datasets/generate_AICUP_patches.py --data_path /content/train
+cd ..
+mkdir yolo
+python yolov7/tools/AICUP_to_YOLOv7.py --AICUP_dir /content/train --YOLOv7_dir /content/yolo
+cd /content/AICUP_Baseline_BoT-SORT
 python3 fast_reid/tools/train_net.py --config-file fast_reid/configs/AICUP/bagtricks_R50-ibn.yml MODEL.DEVICE "cuda:0"
