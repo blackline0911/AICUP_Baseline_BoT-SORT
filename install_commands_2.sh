ba9c49cb2974f83dfb2d6b910146e5b7837fbec0
kevin <99797913+blackline0911@users.noreply.github.com>
2024-05-09 17:25:07 +0800

    Update install_commands_2.sh

--- install_commands_2.sh
+++ install_commands_2.sh
@@ -4,5 +4,8 @@ pip install -r requirements.txt
 pip install cython; pip3 install 'git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI'
 pip install cython_bbox
 pip install faiss-gpu
-python fast_reid/datasets/generate_AICUP_patches.py --data_path /content/drive/MyDrive/train
-python yolov7/tools/AICUP_to_YOLOv7.py --AICUP_dir /content/drive/MyDrive/train --YOLOv7_dir /content/drive/MyDrive/yolo
+cd ..
+mkdir yolo
+cd /content/AICUP_Baseline_BoT-SORT
+python fast_reid/datasets/generate_AICUP_patches.py --data_path /content/train
+python yolov7/tools/AICUP_to_YOLOv7.py --AICUP_dir /content/train --YOLOv7_dir /content/yolo
