408b8c47aec63ca793d5d458547fa2d0272a3d88
kevin <99797913+blackline0911@users.noreply.github.com>
2024-05-12 20:26:46 +0800

    Update install_commands_1.sh

--- install_commands_1.sh
+++ install_commands_1.sh
@@ -1,5 +1,5 @@
 wget https://repo.anaconda.com/archive/Anaconda3-2024.02-1-Linux-x86_64.sh
 bash Anaconda3-2024.02-1-Linux-x86_64.sh
 source ~/.bashrc
-conda create -n bot python=3.7
+source create -n bot python=3.7
 
